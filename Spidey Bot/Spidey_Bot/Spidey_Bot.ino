f9b2c4792a9863031eaf9c500b3d205b0fedadb4
TheOne <collinkuit@gmail.com>
2020-01-20 17:31:20 +0100

    update spidey_bot

--- Spidey Bot/Spidey_Bot/Spidey_Bot.ino
+++ Spidey Bot/Spidey_Bot/Spidey_Bot.ino
@@ -4,17 +4,20 @@
 #define button 5 // button acting as on/off 
 #define trigPin 6 // trig pin on the ultrasoon
 #define echoPin 7 // echo pin on the ultrasoon
+#define ON 15
+#define OFF 16
 
 Servo leftServo;
 Servo rightServo;
-int buttonState = LOW;
-int previousState;
-bool buttonSwitch = false;
+bool buttonState = false;
+bool loopnow;
+bool buttonSwitch = true;
 const int rightAngleMin = 0;
-const int rightAngleMax = 180;
-const int leftAngleMin = 0;
-const int leftAngleMax = 180;
+const int rightAngleMax = 75;
+const int leftAngleMin = 179;
+const int leftAngleMax = 105;
 const int distanceMax = 50;
+int state;
 
 long distance = 0;
 unsigned long timeOut = 5000;
@@ -22,63 +25,79 @@ long pulseTime;
 
 void setup() {
   Serial.begin(9600);
-  leftServo.attach(9);
-  rightServo.attach(10);
+  leftServo.attach(10);
+  rightServo.attach(9);
   leftServo.write(leftAngleMin);
   rightServo.write(rightAngleMin);
   pinMode(button, INPUT);
   pinMode(echoPin, INPUT);
   pinMode(trigPin, OUTPUT);
+  state = OFF;
 
 
 }
 
 void loop() {
-  while ( buttonSwitch == true) {
-    getDistance();
-    if ( distance > 50) {
-      distance = 50;
-    }
-    if (distance != 0) {
-      float motorPosition = map(distance, 10 , 50, leftAngleMax, leftAngleMin);
-      Serial.println(motorPosition);
-      if (distance > 10 && distance != 0) {
-        if (motorPosition > leftAngleMax) {
-          motorPosition = leftAngleMax;
-        }
-        leftServo.write(motorPosition);
-        rightServo.write(motorPosition);
-      } else {
-        leftServo.write(leftAngleMax);
-        leftServo.write(leftAngleMin);
-        rightServo.write(rightAngleMax);
-        rightServo.write(rightAngleMax);
+  switch (state) {
+    case OFF:
+      buttonCheckOFF();
+      break;
+    case ON:
+      getDistance();
+      if ( distance > 50) {
+        distance = 50;
       }
-    }
+      if (distance != 0) {
+        float leftMotorPosition = map(distance, 10 , 50, leftAngleMax, leftAngleMin);
+        float rightMotorPosition = map(distance, 10 , 50, rightAngleMax, rightAngleMin);
+
+        Serial.println(leftMotorPosition);
+        if (distance > 10 && distance != 0) {
+          if (rightMotorPosition > rightAngleMax) {
+            rightMotorPosition = rightAngleMax;
+          }
+          if (leftMotorPosition < leftAngleMax) {
+            leftMotorPosition = leftAngleMax;
+          }
+          leftServo.write(leftMotorPosition);
+          rightServo.write(rightMotorPosition);
+        } else if ( distance < 10) {
+          leftServo.write(leftAngleMax);
+          rightServo.write(rightAngleMax);
+        } else if ( distance == 0) {
+          leftServo.write(leftAngleMin);
+          rightServo.write(rightAngleMin);
+        }
+      }
+      buttonCheckON();
+      break;
   }
 }
-  void buttonCheck() {
-    buttonState = digitalRead(button);
-    if (buttonState != previousState) {
-      previousState = buttonState;
-    }
-    if (buttonState == HIGH) {
-      buttonSwitch = !buttonSwitch;
-    }
-    if ( buttonSwitch == true) {
-      digitalWrite(4, HIGH);
-    } else {
-      digitalWrite(4, LOW);
-    }
+void buttonCheckOFF() {
+  if (digitalRead(button)) {
+    digitalWrite(buttonLED, HIGH);
+    Serial.print("device turned ON");
+    state = ON;
+    delay(500);
   }
+}
 
-  void getDistance() {
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(100);
-    digitalWrite(trigPin, LOW);
-    pulseTime = pulseIn (echoPin, HIGH, timeOut);
-    distance = pulseTime / 58.2;
-    Serial.println(distance);
+void buttonCheckON() {
+  if (digitalRead(button)) {
+    digitalWrite(buttonLED, LOW);
+    Serial.print("device turned OFF");
+    state = OFF;
+    delay(500);
   }
+}
+
+void getDistance() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(100);
+  digitalWrite(trigPin, LOW);
+  pulseTime = pulseIn (echoPin, HIGH, timeOut);
+  distance = pulseTime / 58.2;
+  Serial.println(distance);
+}
